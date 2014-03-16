a8c5cf34c2502e32ec2cf5106e9bc2e5e8a09f55
Timor <timor@cyhex.com>
2014-03-16 08:29:39 +0100

    working arduino

---
+++ Arduino/monitoring/monitor.py
@@ -0,0 +1,57 @@
+import serial
+import time
+import numpy as np
+import matplotlib.pyplot as plt
+import matplotlib.animation as animation
+from collections import defaultdict
+from matplotlib.lines import Line2D
+
+ser = serial.Serial('/dev/ttyACM0',115200,timeout=1)
+sample_len = 50
+
+fig = plt.figure()
+ax0 = fig.add_subplot(1,1,1) # one row, one column, first plot
+ax0.set_ylabel(('value / time'))
+ax0.set_ylim(0, 1024)
+plotter, = ax0.plot([0]*sample_len,color='red')
+ax0.add_line(plotter)
+
+
+def update(data):
+    plotter.set_ydata(data['plot1'])
+    return plotter
+
+def readLine(data):
+    line = ser.readline()
+
+    line  = line.strip()
+  
+    if not line.startswith('plot'):
+        return data
+    
+    bits = line.split(":")
+    if len(bits) == 3:
+        try:
+            data['plot1'].append(float(bits[2]))
+        except Exception, e:
+            return data
+        
+    return data
+
+def data_gen1():
+    data = defaultdict(list)
+    while 1:
+
+        try:
+            data = readLine(data)
+        except Exception, e:
+            continue
+        
+        if len(data['plot1']) == sample_len + 1 :
+            data['plot1'].pop(0)
+            yield data
+        
+   
+ani = animation.FuncAnimation(fig, update, data_gen1, interval=1)
+plt.show()
+
---
+++ Arduino/src/flashReciever/flashReciever.ino
@@ -0,0 +1,125 @@
+// include the library code:
+#include <LiquidCrystal.h>
+// initialize the library with the numbers of the interface pins
+LiquidCrystal lcd(12, 11, 5, 4, 3, 2);
+
+int msDeley = 50;
+int sensorValue = 0;
+int ambientLight = 0;
+
+const boolean debug = false;
+// signal length in ms
+const int signalLenHigh = 100; 
+const int signalLenLow = 50;
+
+// 0 - dark , 1 - light
+int signalStateLast = 0;
+int signalStateCurrent = 0;
+
+long int signalLastTime = 0;
+long int signalLen = 0;
+String inByte = "";
+String recieved = "";
+String lcdLine = "";
+// the setup routine runs once when you press reset:
+void setup() {
+  Serial.begin(115200);
+  lcd.begin(16, 2);
+  // initialize the digital pin as an output.
+  pinMode(A5, INPUT); 
+  pinMode(A4, OUTPUT); 
+  ambientLight = analogRead(A5);
+
+  for(int i=0; i<10; i++){
+      ambientLight = (ambientLight*0.9) + (analogRead(A5)*0.1);
+      delay(100);
+  }
+  ambientLight = max(500, ambientLight);
+  lcdLine = "Ok: (H:";
+  lcdLine += signalLenHigh;
+  lcdLine += " L:";
+  lcdLine += signalLenLow;
+  lcdLine += ")";
+  lcd.print(lcdLine);
+  Serial.println(lcdLine);
+  lcd.setCursor(0, 1);
+  lcdLine = "Ambient: ";
+  lcdLine += ambientLight;
+  lcd.print(lcdLine);
+  Serial.println(lcdLine);
+}
+
+char getBinChar(){
+  if(signalStateLast == 1){
+    // ignore light pulse
+    return '-';
+  }
+
+  if(signalLen >= signalLenLow * 0.3 &&  signalLen <= signalLenLow * 1.3){
+    return '0';
+  }else if(signalLen >= signalLenHigh * 0.8 &&  signalLen <= signalLenHigh * 2){
+    return '1';
+  }else if(signalLen > signalLenHigh*3) {
+    // long  sig - reset 
+    Serial.print("RESET : ");
+    Serial.println(signalLen);
+    recieved = "";
+    inByte = "";
+    return '-';
+  }else{
+    // ignore short signals
+    return '-';
+  }
+
+  
+}
+
+
+// the loop routine runs over and over again forever:
+void loop() {
+  sensorValue = analogRead(A5);
+
+  if(sensorValue >= ambientLight ){
+    signalStateCurrent = 1;
+  }else{
+    signalStateCurrent = 0;
+  }
+
+  if(signalStateCurrent == signalStateLast){
+    signalLen = millis() - signalLastTime;
+  }else{
+    
+    signalLen = millis() - signalLastTime;
+    // signal change
+    char cBit = getBinChar();
+    if(debug){
+      Serial.print(cBit);
+      Serial.print('|');
+      Serial.println(signalLen);
+    }
+    if(cBit != '-'){
+      digitalWrite(A4, 1);
+      inByte += cBit; 
+      lcd.clear();
+      lcd.setCursor(0, 1);
+      lcd.print(inByte);
+    }else{
+      digitalWrite(A4, 0);
+    }
+    
+    signalLastTime = millis();
+    signalStateLast = signalStateCurrent;
+  }
+
+  if(inByte.length() == 8){
+    const char * c = inByte.c_str();
+    recieved += (char) strtol(c, NULL, 2);
+    Serial.println(recieved);
+    lcd.setCursor(0, 0);
+    lcd.clear();
+    lcd.print(recieved);
+    inByte = "";
+  }
+  
+  //delay(1000);
+}
